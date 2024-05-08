32db7477e208ea311f13fae453c03af591c8c522
Joel Eliason <joel.eliason119@gmail.com>
2024-05-08 14:47:48 -0600

    small updates.

--- PSA_NSCLC_iteration.py
+++ PSA_NSCLC_iteration.py
@@ -4,15 +4,23 @@ import pickle as pkl
 import numpy as np
 import argparse
 import pandas as pd
+import os
 
 
 print('Parsing arguments...')
 parser = argparse.ArgumentParser()
 parser.add_argument('--start_index', type=int, default=0)
 parser.add_argument('--n_param_sets', type=int, default=1)
+parser.add_argument('--write_directory', type=str, default='.')
 args = parser.parse_args()
-print('Params row start index: ',args.start_index)
 print('Number of parameter sets: ',args.n_param_sets)
+print('Write directory: ',args.write_directory)
+
+try:
+	start_index = int(os.environ['SLURM_PROCID'])
+except KeyError:
+	start_index = args.start_index
+print('Params row start index: ' + str(start_index))
 
 params_df = pd.read_csv('params_PSA.csv')
 param_names = params_df.columns.values
@@ -28,11 +36,11 @@ eng.addpath('postprocessing/')
 eng.addpath('visualization/')
 
 for i in range(args.n_param_sets):
-	idx = i + args.start_index
+	idx = i + start_index
 	params = params_df.iloc[idx].values
 	param_struct = dict(zip(param_names,params))
 
-	print('Running MATLAB script for set: '+ str(idx))
+	print('Running simulation for set: '+ str(idx))
 	success,index,M1,M2,Teff,T1exh,T0,Th,Thexh,C_total,VDT = eng.PSA_NSCLC_iteration(param_struct,nargout=11)
 
 	print('Saving results for set: ' + str(idx))
@@ -42,7 +50,7 @@ for i in range(args.n_param_sets):
 	result = [success,index] + result + [VDT]
 
 	result_dict = dict(zip(['success','idx','M1','M2','Teff','T1exh','T0','Th','Thexh','C_total','VDT'],result))
-	pkl.dump(result_dict,open('PSA_NSCLC_iteration_' + str(idx) + '.pkl','wb'))
+	pkl.dump(result_dict,open(args.write_directory + '/PSA_NSCLC_iteration_' + str(idx) + '.pkl','wb'))
 
 print('Finished.')
 # M1_M2,Treg_CD8,CD8_CD4 = eng.PSA_NSCLC_iteration(param_struct,nargout=3)
