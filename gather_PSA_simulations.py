db60a2b12024bfadba5c6ba7e3a6cd59c246f4bc
Joel Eliason <joel.eliason119@gmail.com>
2024-06-12 16:55:10 -0600

    gathering.

--- gather_PSA_simulations.py
+++ gather_PSA_simulations.py
@@ -4,6 +4,8 @@ import os
 
 variables = ['M1','M2','Teff','T1exh','T0','Th','Thexh','C_total','VDT']
 
+total_simulations = 600
+
 # Load the results from the PSA simulations
 # get all pkl files
 files = os.listdir('/nfs/turbo/umms-ukarvind/joelne/PSA_simulations/')
@@ -13,26 +15,22 @@ files = [f for f in files if f.endswith('.pkl')]
 with open('/nfs/turbo/umms-ukarvind/joelne/PSA_simulations/' + files[0],'rb') as f:
 	data = pkl.load(f)
 	M1 = data['M1']
+	# variables = data.keys()
 	f.close()
 
 length_var = M1.shape[0]
 
 # initialize numpy array to store results
-results = np.zeros((len(files),len(variables),length_var))
+results = np.zeros((total_simulations,len(variables),length_var))
 
 # load results into numpy array
 for i,f in enumerate(files):
+	sim_number = f.split('_')[-1].split('.')[0]
 	with open('/nfs/turbo/umms-ukarvind/joelne/PSA_simulations/' + f,'rb') as f:
 		data = pkl.load(f)
 		for j,var in enumerate(variables):
-			results[i,j,:] = data[var]
+			results[sim_number,j,:] = data[var]
 		f.close()
 
 # save results
 np.save('/nfs/turbo/umms-ukarvind/joelne/PSA_simulations.npy',results)
-
-# get simulation number from each
-simulations = np.array([f.split('_')[-1].split('.')[0] for f in files])
-
-# save simulation numbers
-np.save('/nfs/turbo/umms-ukarvind/joelne/PSA_simulation_numbers.npy',simulations)
