29c9a71f910b1480e277ff74ca554a794e415615
Joel Nathan Eliason <joelne@gl-login3.arc-ts.umich.edu>
2024-06-12 18:56:09 -0400

    also some updates.

--- gather_PSA_simulations.py
+++ gather_PSA_simulations.py
@@ -1,14 +1,16 @@
 import pickle as pkl
 import numpy as np
 import os
+import matlab.engine
 
-variables = ['M1','M2','Teff','T1exh','T0','Th','Thexh','C_total','VDT']
+variables = ['M1','M2','Teff','T1exh','T0','Th','Thexh','C_total']
 
 # Load the results from the PSA simulations
 # get all pkl files
 files = os.listdir('/nfs/turbo/umms-ukarvind/joelne/PSA_simulations/')
 files = [f for f in files if f.endswith('.pkl')]
 
+print('get data length')
 # get length of M1 from first file
 with open('/nfs/turbo/umms-ukarvind/joelne/PSA_simulations/' + files[0],'rb') as f:
 	data = pkl.load(f)
@@ -19,13 +21,14 @@ length_var = M1.shape[0]
 
 # initialize numpy array to store results
 results = np.zeros((len(files),len(variables),length_var))
-
+print('start reading files')
 # load results into numpy array
 for i,f in enumerate(files):
+	print(i)
 	with open('/nfs/turbo/umms-ukarvind/joelne/PSA_simulations/' + f,'rb') as f:
 		data = pkl.load(f)
 		for j,var in enumerate(variables):
-			results[i,j,:] = data[var]
+			results[i,j,:] = data[var].squeeze()
 		f.close()
 
 # save results
